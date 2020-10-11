66f4d02b5d1ec22fe25700d4efd8836cab70a5d8
muerteloca12 <43545742+muerteloca12@users.noreply.github.com>
2020-10-11 22:20:45 +0200

    Update Shaderx.js

--- Shaderx.js
+++ Shaderx.js
@@ -8,24 +8,12 @@ const button = new PIXI.Graphics()
     .moveTo(36, 30)
     .lineTo(36, 70)
     .lineTo(70, 50);
-// Position the button
+
 button.x = (app.screen.width - button.width) / 2;
 button.y = (app.screen.height - button.height) / 2;
-
-// Enable interactivity on the button
 button.interactive = true;
 button.buttonMode = true;
-
-// Add to the stage
 app.stage.addChild(button);
-
-// Listen for a click/tap event to start playing the video
-// this is useful for some mobile platforms. For example:
-// ios9 and under cannot render videos in PIXI without a
-// polyfill - https://github.com/bfred-it/iphone-inline-video
-// ios10 and above require a click/tap event to render videos
-// that contain audio in PIXI. Videos with no audio track do
-// not have this requirement
 button.on('pointertap', onPlayVideo);
 
 
@@ -62,12 +50,12 @@ PIXI.loader.add('shader', './Shaders/Bruma.frag')
 
 var filter;
 
-// Handle the load completed
+
 function onLoaded(loader, res) {
-    // Create the new filter, arguments: (vertexShader, framentSource)
+  
     filter = new PIXI.Filter(null, res.shader.data);
 
-    // Add the filter
+    /
   let width = window.innerWidth;
   let height = window.innerHeight;
     filter.uniforms.u_resolution= [width, height];
